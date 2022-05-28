b48fcd2a97ef1852cf24fee926e27fa8f07bec60
Mohammad Mahmoud Mohammad EzzEldin <38350499+MohammedEzzEldin@users.noreply.github.com>
2022-05-28 11:04:46 +0200

    Basic Operation
    Create basic structure.
    Install Packages.
    Handle the Layout.

---
+++ POVs.DAL/Database/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POVs.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace POVs.DAL.Database
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer()
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Department> Department { get; set; }
+    }
+}
+
+
