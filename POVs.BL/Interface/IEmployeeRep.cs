ea8957a4df250df1e25a49f362e02f4b3fbb0f51
Mohammad Mahmoud Mohammad EzzEldin <38350499+MohammedEzzEldin@users.noreply.github.com>
2022-06-25 23:48:05 +0200

    create emp rep

---
+++ POVs.BL/Interface/IEmployeeRep.cs
@@ -0,0 +1,17 @@
+﻿using POVs.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace POVs.BL.Interface
+{
+    public interface IEmployeeRep
+    {
+        Task<IEnumerable<Employee>> GetAsync(Expression<Func<Employee, bool>> filter);
+        Task<Employee> GetByIdAsync(int id);
+        Task CreateAsync(Employee employee);
+        Task UpdateAsync(Employee employee);
+        Task DeleteAsync(int employeeId);
+    }
+}
---
+++ POVs.BL/Repository/EmployeeRep.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POVs.BL.Interface;
+using POVs.BL.ModelView;
+using POVs.DAL.Database;
+using POVs.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace POVs.BL.Repository
+{
+    public class EmployeeRep : IEmployeeRep
+    {
+        private readonly ApplicationContext db;
+        public EmployeeRep(ApplicationContext db)
+        {
+            this.db = db;
+        }
+        public async Task CreateAsync(Employee employee)
+        {
+           await db.Employees.AddAsync(employee);
+           await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int employeeId)
+        {
+            var data = await db.Employees.FindAsync(employeeId);
+            db.Remove(data);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> GetAsync(Expression<Func<Employee, bool>> filter)
+        {
+            if(filter != null)
+               return await Task.Run(() => db.Employees.Where(filter).ToList());
+            else 
+               return await Task.Run(() => db.Employees.ToList());
+        }
+
+        public async Task<Employee> GetByIdAsync(int id)
+        {
+            var data = db.Employees.Where(x => x.Id == id).FirstOrDefault();
+
+            return await Task.Run(() => data);
+        }
+
+        public async Task UpdateAsync(Employee employee)
+        {
+            db.Entry(employee).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+        }
+    }
+}
