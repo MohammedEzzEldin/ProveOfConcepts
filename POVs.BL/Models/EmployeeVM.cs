e3ec0a200507f2f0fe3a5e2e20007d9259ee64ad
Mohammad Mahmoud Mohammad EzzEldin <38350499+MohammedEzzEldin@users.noreply.github.com>
2022-06-25 23:34:37 +0200

    create EmployeeVM

--- POVs.BL/Models/EmployeeVM.cs
+++ POVs.BL/Models/EmployeeVM.cs
@@ -1,19 +1,32 @@
-﻿using System;
+﻿using POVs.DAL.Entity;
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-namespace POVs.DAL.Entity
+
+namespace POVs.BL.Models
 {
-    [Table("Employee")]
-    public class Employee
+    public class EmployeeVM
     {
-        [Key]
+        public EmployeeVM()
+        {
+            IsActive = true;
+            IsDeleted = false;
+        }
         public int Id { get; set; }
-        [Required, StringLength(50)]
+        [Required(ErrorMessage = "Please Provide Name!!!")]
+        [MaxLength(50, ErrorMessage = "Max length is 50")]
         public string Name { get; set; }
-        [Required, StringLength(50)]
+        [Required]
+        [EmailAddress(ErrorMessage = "Email invalid")]
         public string Email { get; set; }
+        [
+            RegularExpression(
+            "[1-9]{1-5}-[a-zA-Z]{1-10}-[1-9]{1-5}-[a-zA-Z]{1-10}-[1-9]{1-5}-[a-zA-Z]{1-10}", 
+            ErrorMessage ="Enter like 12-streetname-City-Country"
+           )
+        ]
         public string Address { get; set; }
         public string Notes { get; set; }
+        [Range(2000,10000,ErrorMessage ="Salary btw 2k to 100k")]
         public double? Salary { get; set; }
         public DateTime? HireDate { get; set; }
         public DateTime? DeleteDate { get; set; }
@@ -22,8 +35,6 @@ namespace POVs.DAL.Entity
         public bool IsUpdated { get; set; }
         public bool IsActive { get; set; }
         public int DepartmentId { get; set; }
-        // Navigation Properity
-        [ForeignKey("DepartmentId")]
         public Department Department { get; set; }
     }
 }
