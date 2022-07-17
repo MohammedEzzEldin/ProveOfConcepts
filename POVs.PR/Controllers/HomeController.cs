11378d15f7e6a626076beb41f22077f342b8632d
Mohammad Mahmoud Mohammad EzzEldin <38350499+MohammedEzzEldin@users.noreply.github.com>
2022-07-17 23:15:00 +0200

    test

--- POVs.PR/Controllers/HomeController.cs
+++ POVs.PR/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
+using POVs.PR.Language;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,14 +13,17 @@ namespace POVs.PR.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<ProjectResources> resource;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IStringLocalizer<ProjectResources> Resource)
         {
             _logger = logger;
+            resource = Resource;
         }
 
         public IActionResult Index()
         {
+            ViewBag.Data = resource["DASHBOARD"];
             return View();
         }
     }
