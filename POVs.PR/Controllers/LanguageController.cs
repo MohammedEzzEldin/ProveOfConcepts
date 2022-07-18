c62c19d4eb0527686597c077428b623230d517ee
Mohammad Mahmoud Mohammad EzzEldin <38350499+MohammedEzzEldin@users.noreply.github.com>
2022-07-18 07:35:23 +0200

    remove necessary lib

--- POVs.PR/Controllers/LanguageController.cs
+++ POVs.PR/Controllers/LanguageController.cs
@@ -2,9 +2,6 @@
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace POVs.PR.Controllers
 {
